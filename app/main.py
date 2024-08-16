02ecb75d5785dbff9819eb67cb5ff6edd46ff3ea
prevail <esehgodprevail@gmail.com>
2024-08-16 16:57:27 +0100

    updated the main.py file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import users, auth, movies, ratings, comment
-# import app.models as models
+import app.models as models
 from app.database import engine
 from app.logger import get_logger
 
@@ -9,7 +9,7 @@ from app.logger import get_logger
 logger = get_logger(__name__)
 
 # create database tables based on defined models
-# models.Base.metadata.create_all(bind=engine) -- using alembic to update tables
+models.Base.metadata.create_all(bind=engine)
 
 origins = ["*"]  # Sir this is for development purposes
 
